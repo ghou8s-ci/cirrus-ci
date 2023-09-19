55abdf7d6bc46fe93ec580e4ffa5e46920247a40
ghou8s-ci <136720124+ghou8s-ci@users.noreply.github.com>
2023-09-19 19:26:25 +0700

    ci: update

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/beryllium/qassa-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa-*.zip)"
-      rclone copy ~/rom/out/target/product/beryllium/qassa-*.zip ghou8s-ci:beryllium -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/beryllium/qassa_*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa_*.zip)"
+      rclone copy ~/rom/out/target/product/beryllium/qassa_*.zip ghou8s-ci:beryllium -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa_*.zip) Uploaded Successfully!"
 fi
