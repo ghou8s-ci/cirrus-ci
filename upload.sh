1f75ac7ff4e68606ac0e906c0b35e7dd0d7beb1e
ghou8s-ci <136720124+ghou8s-ci@users.noreply.github.com>
2024-09-25 01:42:12 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/joyeuse/qassa_*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/joyeuse/ && ls qassa_*.zip)"
-      rclone copy ~/rom/out/target/product/joyeuse/qassa_*.zip ghou8s-ci:joyeuse -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/joyeuse/ && ls qassa_*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/joyeuse/Nusantara-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/joyeuse/ && ls Nusantara-*.zip)"
+      rclone copy ~/rom/out/target/product/joyeuse/Nusantara-*.zip gunterez-ci:joyeuse -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/joyeuse/ && ls Nusantara-*.zip) Uploaded Successfully!"
 fi
